0260a626277ecc44fdd2d7030fe858a84b93c6f0
Sushant Pandey <85762925+sephiroth7484@users.noreply.github.com>
2024-10-06 16:36:09 +0530

    Rest API Creation Commit

--- KeepNotes-Clone-backend/src/main/java/org/example/controller/controller.java
+++ KeepNotes-Clone-backend/src/main/java/org/example/controller/controller.java
@@ -5,14 +5,12 @@ import org.example.services.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/KN")
 public class controller {
 
     @Autowired
